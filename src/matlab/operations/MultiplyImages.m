3accde3b776890d49c6f8a94378bf2a2f50d61e8
Juan Carlos Farah <juancarlos.farah@pm.me>
2021-07-20 16:42:54 +0200

    feat: add multiply images with fslmaths operation

---
+++ src/matlab/operations/MultiplyImages.m
@@ -0,0 +1,32 @@
+function [status, result] = MultiplyImages(pathToWorkspace, ...
+                                           params, ...
+                                           config)
+%MULTIPLYIMAGES Multiply two images.
+%   Uses `fslmaths` with `mul` to multiply two images.
+%
+%   Input:
+%   - pathToWorkspace:  Path to the workspace.
+%   - params:           Parameters to be used in the operation.
+%   - config:           Configuration to be used in the operation.
+%
+%   Output:
+%   - status:  Status returned by system call.
+%   - result:  Result returned by system call.
+
+arguments
+  pathToWorkspace string = '.'
+  params.inputFile1 string
+  params.inputFile2 string
+  params.outputFile string
+  config.verbose boolean = false
+end
+
+fullInputFile1 = fullfile(pathToWorkspace, params.inputFile1);
+fullInputFile2 = fullfile(pathToWorkspace, params.inputFile2);
+fullOutputFile = fullfile(pathToWorkspace, params.outputFile);
+
+command = 'fslmaths %s -mul %s %s';
+sentence = sprintf(command, fullInputFile1, fullInputFile2, fullOutputFile);
+[status, result] = CallSystem(sentence, config.verbose);
+
+end
