511e5ffd2f171193535391ed069a791847fa2d0f
Juan Carlos Farah <juancarlos.farah@pm.me>
2021-07-20 16:38:10 +0200

    feat: add fill mask with fslmaths operation

---
+++ src/matlab/operations/FillMask.m
@@ -0,0 +1,31 @@
+function [status, result] = FillMask(pathToWorkspace, ...
+                                     params, ...
+                                     config)
+%FILLMASK Fill holes in the brain mask, without changing fov.
+%   Uses `fslmaths` with `fillh` to fill holes in a mask.
+%
+%   Input:
+%   - pathToWorkspace:  Path to the workspace.
+%   - params:           Parameters to be used in the operation.
+%   - config:           Configuration to be used in the operation.
+%
+%   Output:
+%   - status:  Status returned by system call.
+%   - result:  Result returned by system call.
+
+arguments
+  pathToWorkspace string = '.'
+  params.inputFile string
+  params.outputFile string
+  config.verbose boolean = false
+end
+
+fullInputFile = fullfile(pathToWorkspace, params.inputFile);
+fullOutputFile = fullfile(pathToWorkspace, params.outputFile);
+command = 'fslmaths %s -fillh %s';
+sentence = sprintf(command, fullInputFile, fullOutputFile);
+  
+[status, result] = CallSystem(sentence, config.verbose);
+
+end
+
