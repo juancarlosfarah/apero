108ba4f2810ed202cfbd39b8f41ed1b3d2c8bc1d
Juan Carlos Farah <juancarlos.farah@epfl.ch>
2021-07-30 13:42:29 +0200

    fix: fix verbose issue and add options

--- src/matlab/operations/MultiplyImages.m
+++ src/matlab/operations/MultiplyImages.m
@@ -18,8 +18,17 @@ arguments
   params.inputFile1 char
   params.inputFile2 char
   params.outputFile char
+  % optional step
+  config.optional logical = false
+  % clobber previous output
+  config.clobber logical = false
+  % switch on diagnostic messages
   config.verbose logical = false
+  config.v logical = false
 end
+
+% normalize if multiple options mean the same thing
+verbose = config.verbose || config.v;
 
 fullInputFile1 = fullfile(pathToWorkspace, params.inputFile1);
 fullInputFile2 = fullfile(pathToWorkspace, params.inputFile2);
@@ -27,6 +36,6 @@ fullOutputFile = fullfile(pathToWorkspace, params.outputFile);
 
 command = 'fslmaths %s -mul %s %s';
 sentence = sprintf(command, fullInputFile1, fullInputFile2, fullOutputFile);
-[status, result] = CallSystem(sentence, config.verbose);
+[status, result] = CallSystem(sentence, verbose);
 
 end
