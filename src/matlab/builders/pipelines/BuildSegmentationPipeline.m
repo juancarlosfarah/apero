0c281e7e6e02d13eda6029e775496a1bb05fceae
Juan Carlos Farah <juancarlos.farah@epfl.ch>
2021-08-03 16:38:44 +0200

    feat: start work on the segmentation pipeline

---
+++ src/matlab/builders/pipelines/BuildSegmentationPipeline.m
@@ -0,0 +1,81 @@
+function [pipeline] = BuildSegmentationPipeline(pathToWorkspace, ...
+                                                pathToDataset, ...
+                                                pathToOutputsFromPreviousPipelines, ...
+                                                pathToOutput, ...
+                                                numSubjects, ...
+                                                config)
+%BUILDSEGMENTATIONPIPELINE Example of a pipeline builder for segmentation.
+%   This builder creates a pipeline with one sequence per subject, based
+%   on the format of a BIDS dataset's participants.tsv file.
+%   
+%   Input:
+%   - pathToWorkspace:  Path to the workspace.
+%   - pathToDataset:    Path to input the root of the BIDS dataset.
+%   - pathToOutput:     Path to where we will output the data.
+%
+%   Output:
+%   - pipeline:  Built pipeline.
+
+arguments
+  pathToWorkspace char = '.'
+  pathToDataset char = '.'
+  pathToOutputsFromPreviousPipelines char = '.'
+  pathToOutput char = '.'
+  numSubjects int8 {mustBeNonnegative} = 0
+  config = {}
+end
+
+% names of inputs needed to start the sequence
+inputs = { ...
+  fullfile(pathToOutputsFromPreviousPipelines, '%s/%s_T1w_brain_mul.nii.gz'), ...
+  fullfile(pathToOutputsFromPreviousPipelines, '%s/T1_subcort_seg.nii.gz'), ...
+};
+numInputs = length(inputs);
+
+% get information about participants
+subjects = readtable(fullfile(pathToDataset, 'participants.tsv'), ...
+                     'Delimiter', ...
+                     '\t', ...
+                     'FileType', ...
+                     'delimitedtext', ...
+                     'PreserveVariableNames', ...
+                     true);
+
+tableHeight = height(subjects);
+if ~numSubjects
+  numSubjects = tableHeight;
+else
+  numSubjects = min(numSubjects, tableHeight);
+end
+
+sequences = cell(1, numSubjects);
+
+% create a sequence for each subject
+for i = 1 : numSubjects
+  % get data for one participant
+  subject = subjects(i, :);
+
+  % participant_id is the column name and it's a cell
+  subjectName = subject.participant_id{1};
+
+  % create an input array for each sequence
+  subjectInputs = cell(1, numInputs);
+  for j = 1 : numInputs
+    input = inputs{j};
+    subjectInputs{j} = sprintf(input, subjectName);
+  end
+
+  pathToSubjectWorkspace = fullfile(pathToWorkspace, subjectName);
+  pathToSubjectOutput = fullfile(pathToOutput, subjectName);
+  sequences{i} = BuildSegmentationSequence(subjectInputs, ...
+                                           subjectName, ...
+                                           pathToSubjectWorkspace, ...
+                                           pathToSubjectOutput, ...
+                                           config);
+end
+
+% create a pipeline with the sequences
+parallel = config.parallel;
+pipeline = Pipeline(sequences, parallel);
+
+end
