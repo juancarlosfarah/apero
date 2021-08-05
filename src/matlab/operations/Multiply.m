12f4e9484b908d859690ccb0cee1f1536e448cd1
Juan Carlos Farah <juancarlos.farah@epfl.ch>
2021-08-05 16:31:52 +0200

    feat: add multiply operation

---
+++ src/matlab/operations/Multiply.m
@@ -0,0 +1,37 @@
+function [status, result] = Multiply(pathToWorkspace, ...
+                                     params, ...
+                                     config)
+%MULTIPLY Multiply a volume by a factor.
+%   Uses `fslmaths` with `mul` to multiply a volume by a factor.
+%
+%   Input:
+%   - pathToWorkspace:  Path to the workspace.
+%   - params:           Parameters to be used in the operation.
+%   - config:           Configuration to be used in the operation.
+%
+%   Output:
+%   - status:  Status returned by system call.
+%   - result:  Result returned by system call.
+
+arguments
+  pathToWorkspace char = '.'
+  params.inputVolume char
+  params.outputVolume char
+  % factor to multiply by
+  params.factor double
+  % switch on diagnostic messages
+  config.verbose logical = false
+  config.v logical = false
+end
+
+% normalize if multiple options mean the same thing
+verbose = config.verbose || config.v;
+
+fullInputVolume = fullfile(pathToWorkspace, params.inputVolume);
+fullOutputVolume = fullfile(pathToWorkspace, params.outputVolume);
+
+command = 'fslmaths %s -mul %d %s';
+sentence = sprintf(command, fullInputVolume, params.factor, fullOutputVolume);
+[status, result] = CallSystem(sentence, verbose);
+
+end
